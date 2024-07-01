f11b0b9bfda892dc005852eef12b083c3c3feb14
therealloft <stephenpadgham@yahoo.co.uk>
2024-07-01 23:32:14 +0100

    Added Cors Policy and corrected jsonp format

--- FetchMyIp.Api/Program.cs
+++ FetchMyIp.Api/Program.cs
@@ -1,37 +1,44 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 app.UseHttpsRedirection();
+app.UseCors();
 
 app.MapGet("", (HttpContext context, string? format, string? callback) =>
 {
-    var ipAddress = context.Connection.RemoteIpAddress;
-    var ipv4Address = ipAddress != null && ipAddress.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork
-        ? ipAddress.ToString()
-        : null;
-
-    if (ipv4Address == null)
-        return Results.BadRequest("No IPv4 address found");
+    var ipAddress = context.Connection.RemoteIpAddress?.ToString();
+    if (string.IsNullOrEmpty(ipAddress))
+        return Results.BadRequest("No IP address found");
 
     if (string.IsNullOrEmpty(format) && string.IsNullOrEmpty(callback))
-        return Results.Text(ipv4Address);
+        return Results.Text(ipAddress);
 
     if (!string.IsNullOrEmpty(format))
     {
         switch(format.ToLower())
         {
             case "json":
-                return Results.Json(new { ip = ipv4Address });
+                return Results.Json(new { ip = ipAddress });
 
             case "xml":
                 context.Response.ContentType = "application/xml";
-                return Results.Text($"<ip>{ipv4Address}</ip>", "application/xml");
+                return Results.Text($"<ip>{ipAddress}</ip>", "application/xml");
 
             case "jsonp":
                 if (string.IsNullOrEmpty(callback))
-                    return Results.Text($"callback({{\"ip\":\"{ipv4Address}\"}})");
+                    return Results.Text($"callback({{\"ip\":\"{ipAddress}\"}})");
 
                 if (!string.IsNullOrEmpty(callback) && callback.Length > 0)
-                    return Results.Text($"{callback}({{\"ip\":\"{ipv4Address}\"}})");
+                    return Results.Text($"{callback}({{\"ip\":\"{ipAddress}\"}})");
 
                 return Results.BadRequest("Invalid format");
         }
